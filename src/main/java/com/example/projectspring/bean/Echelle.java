823824c996076eec125080a01f78b01d5d728fca
Soufiane <123811433+SoufianeBenseddiq@users.noreply.github.com>
2024-11-11 19:47:19 +0100

    soufiane beans

---
+++ src/main/java/com/example/projectspring/bean/Echelle.java
@@ -0,0 +1,56 @@
+package projet.mission.project1.bean;
+
+import jakarta.persistence.*;
+import projet.mission.project1.bean.Grade;
+
+@Entity
+public class Echelle {
+    @Id@GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String libelle;
+    private String code;
+    private Long numero;
+
+    @ManyToOne
+    private Grade grade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Long numero) {
+        this.numero = numero;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Grade grade) {
+        this.grade = grade;
+    }
+}
